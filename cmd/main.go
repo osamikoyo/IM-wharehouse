f0f76d8fd20db66d99161eb6c6de6cd77bd8e578
osamikoyo <v.pylayev@mail.ru>
2025-02-19 21:05:18 +0500

    add app

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/osamikoyo/IM-wharehouse/internal/app"
+)
+
+func main() {
+	app,err := app.New()
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	if err = app.Run(); err != nil{
+		fmt.Println(err)
+	}
+}
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,46 @@
 package app
+
+import (
+	"fmt"
+
+	"github.com/osamikoyo/IM-wharehouse/internal/rpc/server"
+	"github.com/osamikoyo/IM-wharehouse/pkg/config"
+	"github.com/osamikoyo/IM-wharehouse/pkg/loger/loger"
+)
+
+type App struct{
+	loger loger.Logger
+	rpcServer *server.RpcServer
+	cfg *config.Config
+}
+
+func New() (*App, error){
+	cfg, err := config.Load("config.yml")
+	if err != nil{
+		return nil, err
+	}
+
+	serv, err := server.New(cfg)
+	if err != nil{
+		return nil, err
+	}
+
+	return &App{
+		loger: loger.New(),
+		rpcServer: serv,
+		cfg: cfg,
+	},nil
+}
+
+func (a *App) Run() error {
+	a.loger.Info().Msg("starting rpc server...")
+
+	err := a.rpcServer.Run()
+	if err != nil{
+		return fmt.Errorf("error started server: %v", err)
+	}
+
+	a.loger.Info().Msg("Server started succesfully!")
+	return nil
+}
+
