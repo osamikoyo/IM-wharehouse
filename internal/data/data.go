87c68346de814c565ef57e4719b3e7fc9e8b9da1
osamikoyo <v.pylayev@mail.ru>
2025-02-19 06:36:29 +0500

    add rezerv

--- internal/data/data.go
+++ internal/data/data.go
@@ -1,8 +1,45 @@
 package data
 
-import "gorm.io/gorm"
+import (
+	"github.com/osamikoyo/IM-wharehouse/internal/config"
+	"github.com/osamikoyo/IM-wharehouse/internal/data/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"time"
+)
 
 type Storage struct {
 	db *gorm.DB
 }
 
+func New(cfg *config.Config) (*Storage, error){
+	db, err := gorm.Open(sqlite.Open(cfg.DSN))
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&models.Product{})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Storage{
+		db: db,
+	}, nil
+}
+
+func (s *Storage) RezervProduct(name string) error {
+	var product models.Product
+
+	result := s.db.Where(&models.Product{Name: name}).Find(&product)
+	if result.Error != nil{
+		return result.Error
+	}
+
+	result = s.db.Model(&product).Update("Count", name)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
